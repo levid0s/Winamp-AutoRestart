7bb17b3678f5873814e9ef91c097f8917dc4405d
lev <lev@me.com>
2023-03-22 22:57:48 +0000

    checkpoint

--- Start-WinampAutoRestart.ps1
+++ Start-WinampAutoRestart.ps1
@@ -1,15 +1,22 @@
+<#PSScriptInfo
+.VERSION      2023.03.22
+.AUTHOR       Levente Rog
+.COPYRIGHT    (c) 2023 Levente Rog
+.LICENSEURI   https://github.com/levid0s/Winamp-AutoRestart/blob/master/LICENSE.md
+.PROJECTURI   https://github.com/levid0s/Winamp-AutoRestart/
+#>
+
 <#
   .SYNOPSIS
-  Script for monitoring Winamp in the background, and if playback has been paused/stopped for `$FlushAfterSeconds`, it will restart Winamp, to get the Media Library written to disk.
+  Script for monitoring Winamp, and restart it when idle, to get the Media Library written to disk.
   
   .DESCRIPTION
-  The WinAmp Media Library database is only saved when Winamp is closed. If Winamp has been open for a long time (I sometimes have it for days), and later the app crashes, all those changes will be lost.
-  This script keeps monitoring Winamp in the background, using the Windows API, and if the plaback is paused/stopped for `$FlushAfterSeconds`, the app will be restarted. Playback will be skeed to the same position from before the app restart.
+  The WinAmp Media Library database is only saved when Winamp is closed. If Winamp has been open for a long time, and later the app crashes, those changes will be lost. (I usually keep Winamp open for days)
+  This script keeps monitoring Winamp in the background, using the Windows API, and if the plaback is paused/stopped for `$FlushAfterSeconds`, the app will be restarted. Playback will be seeked back to the same position, as before the restart.
 
   .PARAMETER FlushAfterSeconds
-  The number of seconds to wait before restarting Winamp, if playback has been paused/stopped. Default is 300 seconds (5 minutes).
-  This is only counted from the moment when the playback was stopped.
-  When a song is playing no action will be taken, and the timer will be reset.
+  Restart Winamp after it has been idle for this number of seconds. Default is 300 seconds (5 minutes).
+  Idle means plackback is either stopped or paused.
 
   .PARAMETER LogLevel
   The level of logging to write to the Log file. Default is 'Information'.
@@ -19,11 +26,15 @@
   Install the script as a Windows Scheduled Task, to run at startup.
   AllUsers requires the script to be run as Administrator (although the Scheduled task will be started as limited privileges, as `BUILTIN\Users`).
   A detailed Log file is written to `%TEMP%\Start-WinampAutoFlush.ps1-%TIMESTAMP%.log`
-  **CAVEATS**: Stopping the Scheduled Task doesn't also kill the running PowerShell script; You'll have to kill it manually using the Task manager.
+  **CAVEATS**: Stopping the Scheduled Task doesn't kill the running PowerShell script; You'll have to kill it manually using the Task manager.
 
   .PARAMETER Uninstall
   Remove the previously created Windows Scheduled Task
+
+  .TODO
+  - It would be more efficient to start the task at logon, instead of every 15 minutes.
 #>
+
 param(
   [int]$FlushAfterSeconds = 300,
   [Parameter()][ValidateSet('Verbose', 'Debug', 'Information')][string]$LogLevel = 'Information',
